01166de24bf7099dea0ad3730a98d9c4e3d2423e
Gerard <freelancergerard@gmail.com>
2018-09-01 13:00:18 +0530

    Socket Connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
 const path=require('path');
+const http=require('http');
 const express=require('express');
+const socketIO=require('socket.io');
+
 
 const publicPath=path.join(__dirname,'../public');
 const port=process.env.PORT || 3000;
 var app=express();
+var server=http.createServer(app);
+var io=socketIO(server);
 
 app.use(express.static(publicPath));
+
+io.on('connection',(socket)=>{
+	
+	console.log('New User Connected to Our App');
+	socket.on('disconnect',()=>{
+		console.log("User Disconnected from Server");
+	
+	})	
+});
+
 
 
 /* app.get('/',(req,res)=>{
@@ -16,7 +31,7 @@ app.use(express.static(publicPath));
 }); */
 
 
-app.listen(port,()=>{
+server.listen(port,()=>{
 	
 	console.log(`Server is Up on Port ${port}`);
 	
